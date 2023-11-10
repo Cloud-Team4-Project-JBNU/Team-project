2472d47a41fd71aa151109ef7e5b9e362ce98652
Koh-Du-Beom <beanleaf3260@gmail.com>
2023-11-10 16:31:14 +0900

    recovered

--- my-app/src/component/page/utilites/UserMenu.jsx
+++ my-app/src/component/page/utilites/UserMenu.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import styled from "styled-components";
+import UtilNavBar from './ui/UtilNavBar';
 
 function UserMenu(){
   const navigate = useNavigate();
@@ -10,6 +11,7 @@ function UserMenu(){
 
   return (
     <div>
+      <UtilNavBar/>
       기능구현 어떻게 할지 정해봐야함.
     </div>
   )
