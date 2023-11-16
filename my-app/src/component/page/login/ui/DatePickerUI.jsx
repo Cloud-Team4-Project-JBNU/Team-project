068925996dd7f9e3e397705957afbe8dd4005a89
Koh-Du-Beom <140967208+Koh-Du-Beom@users.noreply.github.com>
2023-11-17 01:33:19 +0900

    filtering remove

--- my-app/src/component/page/login/ui/DatePickerUI.jsx
+++ my-app/src/component/page/login/ui/DatePickerUI.jsx
@@ -13,81 +13,54 @@ const StyledDatePicker = styled(DatePicker)`
   border-radius: 4px;
 `;
 
-const StyledSelect = styled.select`
-  padding: 5px;
-  margin : 5px;
-  &: hover{
-    cursor: pointer;
-    background-color: #f2f2f2;
-  }
-`
-
-const CustomHeader = ({ date, changeYear, changeMonth, setViewingMonth}) => {
+const CustomHeader = ({ date, changeYear, changeMonth }) => {
   // 연도와 월을 위한 범위 설정
   const years = Array.from(new Array(100), (val, index) => new Date().getFullYear() - index);
   const months = Array.from({length: 12}, (item, index) => ko.localize.month(index, { width: 'abbreviated' }));
   
-  const handleMonthChange = (e) => {
-    const newMonth = Number(e.target.value);
-    changeMonth(newMonth);
-    setViewingMonth(newMonth);
-  }
-
   return (
     <div style={{ margin: 10 }}>
-      <div>날짜를 선택해주세요</div>
-      <StyledSelect
+      <select
         value={date.getFullYear()}
-        onChange={({ target: { value }}) => changeYear(Number(value))}
+        onChange={({ target: { value }}) => changeYear(value)}
       >
         {years.map(option => (
           <option key={option} value={option}>
             {option}
           </option>
         ))}
-      </StyledSelect>
+      </select>
 
-      <StyledSelect
-        value={date.getMonth()}
-        onChange={({ target: { value }}) => changeMonth(Number(value))}
+      <select
+        value={months[date.getMonth()]}
+        onChange={({ target: { value }}) => changeMonth(months.indexOf(value))}
       >
         {months.map((option, index) => (
           <option key={index} value={index}>
             {option}
           </option>
         ))}
-      </StyledSelect>
+      </select>
     </div>
   );
 };
 
 function DatePickerUI(props) {
   const { selected, onChange, dateFormat, locale } = props;
-  const [viewingMonth, setViewingMonth] = useState(selected.getMonth());
-  const filterDate = (date) => {
-    //선택한 월과 같은 월의 날짜만 true를 반환 
-    return date.getMonth() === viewingMonth;
-  }
-
   return (
     <StyledDatePicker
       selected={selected}
-      onChange={(date)=>{
-        onChange(date);
-        setViewingMonth(date.getMonth());
-      }}
+      onChange={onChange}
       dateFormat={dateFormat}
       locale={locale}
-      filterDate={filterDate}
       renderCustomHeader={props => (
-        <CustomHeader {...props} setViewingMonth={setViewingMonth} />
+        <CustomHeader {...props} />
       )}
       showMonthDropdown
       showYearDropdown
-      dropdownMode="select" 
+      dropdownMode="select" // 이 부분을 추가하여 드롭다운으로 년도와 월을 선택할 수 있게 합니다.
     />
   );
 }
 
 export default DatePickerUI;
-
