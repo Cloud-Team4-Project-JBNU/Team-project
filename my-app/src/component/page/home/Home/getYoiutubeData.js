e19ce8f8d31945c891ff02b7c34e5313b622b9b7
Koh-Du-Beom <140967208+Koh-Du-Beom@users.noreply.github.com>
2023-12-06 21:13:32 +0900

    Update getYoiutubeData.js

--- my-app/src/component/page/home/Home/getYoiutubeData.js
+++ my-app/src/component/page/home/Home/getYoiutubeData.js
@@ -1,27 +1,44 @@
-const getYoutubeData = [
-  {
-    id: 1,
-    videoId: 'Y1FbowQRcmI',
-    title: '데이터 정규화가 뭔지 설명해보세요(개발면접타임)',
-    uploader: '코딩애플',
-    viewCount: '21.7만명',
-  },
-  {
-    id: 2,
-    
-  },
-  {
-    id: 3,
-    
-  },
-  {
-    id: 4,
-    
-  },
-  {
-    id: 5,
-    
-  },
-]
+const getYoutubeData = () => {
+  return {
+    longData : [
+      {
+        id: 1,
+        videoId: 'Y1FbowQRcmI',
+        title: '데이터 정규화가 뭔지 설명해보세요(개발면접타임)',
+        uploader: '코딩애플',
+        viewCount: '21.7만명',
+      },
+      {
+        id: 2,
+        
+      },
+      {
+        id: 3,
+        
+      },
+      {
+        id: 4,
+        
+      },
+      {
+        id: 5,
+        
+      },
+    ],
+    shortsData : [
+      {
+        id: 1,
+        videoId: 'Y1FbowQRcmI',
+        title: '데이터 정규화가 뭔지 설명해보세요(개발면접타임)',
+        uploader: '코딩애플',
+        viewCount: '21.7만명',
+      },
+    ]
+
+
+  }
+  
+  
+}
 
 export default getYoutubeData;
